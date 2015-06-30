dfd4760b4d9c95b3d48b1aa4a372ce59b4117821
duyngha <duyngha@gmail.com>
2015-06-30 22:33:04 +0700

    fixed empty key issue and select all current keyword

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,42 +1,47 @@
 jQuery(document).ready(function($){
 	var $ = jQuery;
 
+	result_box = $('#wpns_results_box');
+	loading = $('#wpns_loading_search');
+	search_icon = $('#wpns_search_icon');
+	compare_key = '';
 	/* event occurs when people type */
 	$('#wpns_search_input').keyup(function(){
 
-		$('#wpns_results_box').hide();
+		result_box.hide();
 		$('.wpns_results_list').remove();
 
 		var keyword = $(this).val();
 
-		/* Show/Hide loading and search icon */
-		$('#wpns_loading_search').show();
-		$('#wpns_search_icon').hide();
+		if (keyword != '' && keyword != compare_key) {
+			/* Show/Hide loading and search icon */
+			loading.show();
+			search_icon.hide();
 
-		/* */
+			/* delay keyup event */
+		    delay(function(){
+		    	
+					var data = {
+						action : 'wpns_search_ajax',
+						keyword : keyword,
+					};
+					$.post(wpns_ajax_url.ajaxurl, data, function(response){
+						loading.hide();
+						search_icon.show();
+						result_box.append(response).slideDown(300);
+					});
+					compare_key = keyword;
 
-	    delay(function(){
-			var data = {
-				action : 'wpns_search_ajax',
-				keyword : keyword,
-			};
-			$.post(wpns_ajax_url.ajaxurl, data, function(response){
-				$('#wpns_loading_search').hide();
-				$('#wpns_search_icon').show();
-
-				$('#wpns_results_box').append(response).slideDown(300);
-				//alert(response);
-
-			});
-	    }, 1000 );	
-
+		    }, 1000 );
+	    }
+	    
 		return false;
 	});
 	
 
 	/* Hide results box when click around the box */
 	$(':not(#wpns_results_box)').click(function(){
-		$('#wpns_results_box').hide();
+		result_box.hide();
 	});
 	
 	/* Set delay time and clear settimeout */
