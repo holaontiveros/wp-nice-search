4195ff269511f39fd733e9328b64551d2956c49d
duyngha <duyngha@gmail.com>
2015-07-01 14:47:41 +0700

    change register style path

--- src/core/admin.php
+++ src/core/admin.php
@@ -18,7 +18,7 @@ class WPNS_Admin {
 	 * function callback to enqueue scripts and style
 	 */
 	public function wpns_plugin_script() {
-		wp_enqueue_style( 'wpns-style', WPNS_URL . 'assets/css/css/style.css' );
+		wp_enqueue_style( 'wpns-style', WPNS_URL . 'assets/css/style.min.css' );
 		wp_enqueue_style( 'wpns-fontawesome', WPNS_URL . 'assets/css/font-awesome.min.css' );
 	}
 
