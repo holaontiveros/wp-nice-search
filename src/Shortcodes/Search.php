b2e1851d9d368499be539a8282cc9470ddc2b830
duyngha <duyngha@gmail.com>
2016-06-06 17:10:07 +0700

    add new shortcode object

---
+++ src/Shortcodes/Search.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Create search shortcode
+ * 
+ * @package wpnt
+ * @since   1.1.0
+ * @author  Duy Nguyen <duyngha@gmail.com>
+ */
+
+namespace WPNS\Shortcodes;
+
+use Shortcode\Classes\Shortcode as Shortcode;
+use Corcel\Options as Options;
+
+class Search extends Shortcode
+{
+	public function __construct()
+	{
+		$this->shortcodeName = 'wpns_search_form_test';
+
+		$this->setPlatesPath(WPNS_DIR . '/templates');
+
+		$this->setTemplate('search');
+
+		$this->setPlates();
+
+	}
+
+	public function callback($atts, $cont)
+	{
+		$options = shortcode_atts(
+			array(
+				'only_search' => '',
+			),
+			$atts
+		);
+
+		$settings = Options::get('wpns_options');
+
+		echo $this->plates->render($this->templateName, ['options' => $options, 'settings' => $settings]);
+	}
+}
