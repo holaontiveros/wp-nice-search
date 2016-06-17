4f12781c8011ed04b010c03cea10cdb00fdc3f79
duyngha <duyngha@gmail.com>
2016-06-17 16:41:40 +0700

    change namespace for core/

--- src/core/Results/ResultCase/DefaultResult.php
+++ src/core/Results/ResultCase/DefaultResult.php
@@ -1,7 +1,7 @@
 <?php
-namespace core\Results\ResultCase;
+namespace WPNS\core\Results\ResultCase;
 
-use core\Results\Results as Results;
+use WPNS\core\Results\Results as Results;
 
 /**
  * This class create a default list with title and icons
